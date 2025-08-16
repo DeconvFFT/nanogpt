e708df97f9021ada79a0ba613d2f3dcc09f6997e
DeconvFFT <mehta.saumya29@gmail.com>
2025-08-16 00:03:48 -0400

    bug fixes

--- scripts/bigram.py
+++ scripts/bigram.py
@@ -281,7 +281,7 @@ for iter in range(max_iters):
 
 context = torch.zeros((1,1), dtype=torch.long, device=device)
 
-print(decode(m.generate(context, max_new_tokens=500)[0].tolist()))
+print(decode(model.generate(context, max_new_tokens=500)[0].tolist()))
     
     
     

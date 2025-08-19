faedc026720eec386810a022ed37e42a7ca3664e
DeconvFFT <mehta.saumya29@gmail.com>
2025-08-19 18:26:16 -0400

    Successful run on multi GPU

--- scripts/train.py
+++ scripts/train.py
@@ -67,38 +67,32 @@ def check_ddp_env():
     
     # setting up ddp
     # torchun command sets the env variables RANK, LOCALRANK AND WORLD_SIZE
-    ddp = int(os.environ.get('RANK',-1)) != -1
+    ddp = int(os.environ.get('RANK', -1)) != -1 # is this a ddp run?
     if ddp:
-        
-        # use DDP if CUDA is available        
-        if torch.cuda.is_available():
-            init_process_group(backend='nccl')
-            torch.cuda.set_device(int(os.environ.get('LOCAL_RANK',0)))
-        elif torch.backends.mps.is_available():
-            init_process_group(backend='mps')
-            torch.mps.set_device(int(os.environ.get('LOCAL_RANK',0)))
-        else:
-            raise ValueError("DDP requires CUDA or MPS")
-        
+        # use of DDP atm demands CUDA, we set the device appropriately according to rank
+        assert torch.cuda.is_available(), "for now i think we need CUDA for DDP"
+        init_process_group(backend='nccl')
         ddp_rank = int(os.environ['RANK'])
         ddp_local_rank = int(os.environ['LOCAL_RANK'])
         ddp_world_size = int(os.environ['WORLD_SIZE'])
-        ddp_device = f'cuda:{ddp_local_rank}' if torch.cuda.is_available() else 'mps' if torch.backends.mps.is_available() else 'cpu'
-        if ddp_device == 'cuda':
-            torch.cuda.set_device(ddp_local_rank)
-        elif ddp_device == 'mps':
-            torch.mps.set_device(ddp_local_rank)
-        else:
-            raise ValueError("DDP requires CUDA or MPS")
-        master_process = ddp_rank == 0
+        device = f'cuda:{ddp_local_rank}'
+        torch.cuda.set_device(device)
+        master_process = ddp_rank == 0 # this process will do logging, checkpointing etc.
     else:
+        # vanilla, non-DDP run
         ddp_rank = 0
         ddp_local_rank = 0
         ddp_world_size = 1
-        ddp_device = detect_device()
         master_process = True
+        # attempt to autodetect device
+        device = "cpu"
+        if torch.cuda.is_available():
+            device = "cuda"
+        elif hasattr(torch.backends, "mps") and torch.backends.mps.is_available():
+            device = "mps"
+        print(f"using device: {device}")
     
-    return ddp, ddp_rank, ddp_local_rank, ddp_world_size, ddp_device, master_process
+    return ddp, ddp_rank, ddp_local_rank, ddp_world_size, device, master_process
     
 def train_model(model,raw_model, ddp, ddp_rank, ddp_local_rank, ddp_world_size, ddp_device, master_process, device):
 
